d2767e235207994cf5c2d68780916878ec57aabf
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-07-31 20:21:26 +0900

    再度FizzBuzz

--- fizzbuzz/FizzBuzz4.java
+++ fizzbuzz/FizzBuzz4.java
@@ -1,13 +1,7 @@
-public class FizzBuzz {
+public class FizzBuzz4 {
 	public static void main(String[] args) {
-		//FizzBuzz
-		/*「1から100までの数字を画面に表示する。ただし、3の倍数のときは数字の代わりにFizzと表示し、5の倍数のときは数字の代わりにBuzzと表示し、
-		  15の倍数のときは数字の代わりにFizzBuzzと表示する」 */
-		
-		//while
-		int i = 0;
-		while (i < 100) { //100以下ならばブロック内を実行するから99まで行う
-			i++; //99に+1するので100が最後になる
+		//for
+		for (int i = 1; i <= 100; i++) {
 			if (i % 15 == 0) {
 				System.out.println("FizzBuzz");
 			} else if (i % 5 == 0) {
@@ -19,6 +13,7 @@ public class FizzBuzz {
 			}
 		}
 		
+		System.out.println();
 		
 		//do while
 		int j = 0;
@@ -35,19 +30,21 @@ public class FizzBuzz {
 			}
 		} while (j < 100);
 		
+		System.out.println();
 		
-		//for
-		for (int k = 1; k <= 100; k++) {
+		//while
+		int k = 0;
+		while (k < 100) {
+			k++;
 			if (k % 15 == 0) {
 				System.out.println("FizzBuzz");
 			} else if (k % 5 == 0) {
 				System.out.println("Buzz");
-			} else if (k % 3 == 0) {
+			} else if (k % 3== 0) {
 				System.out.println("Fizz");
 			} else {
 				System.out.println(k);
 			}
 		}
-		
 	}
 }
