045e28086fc12859369e2a6e119ca6b2d5cdf760
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-08-04 22:19:52 +0900

    FizzBuzz5

---
+++ fizzbuzz/FizzBuzz5.java
@@ -0,0 +1,46 @@
+public class FizzBuzz5 {
+	public static void main(String[] args) {
+		//for
+		for (int i = 1; i <= 100; i++) {
+			if (i % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(i);
+			}
+		}
+		
+		//do while
+		int j = 0;
+		do {
+			j++;
+			if (j % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (j % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (j % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(j);
+			}
+		} while (j < 100);
+		
+		//while
+		int k = 0;
+		while (k < 100) {
+			k++;
+			if (k % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (k % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (k % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(k);
+			}
+		}
+	}
+}
